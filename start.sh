cf938d4d24c6046416b40b3067d57db8151a532d
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2023-06-22 00:03:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kevinnadar22/URL-Shortener-V2.git /URL-Shortener-V2
+  git clone https://github.com/NikhilNGY/Shortner-Converter-Bot-V2.git /Shortner-Converter-Bot-V2
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener-V2
+  git clone $SOURCE_CODE /Shortner-Converter-Bot-V2
 fi
-cd /URL-Shortener-V2
+cd /Shortner-Converter-Bot-V2
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
