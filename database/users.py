f514fccbe4358e59fd63d89642decff9203b3f02
Akhand Bharat <125857863+Singhsawan@users.noreply.github.com>
2024-10-20 14:21:48 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -18,7 +18,7 @@ async def get_user(user_id):
             "header_text": "",
             "footer_text": "",
             "username": None,
-            "base_site": "omnifly.in.net",
+            "base_site": "Tryshort.in",
             "banner_image": None,
             "is_banner_image": True,
             "is_username": True,
